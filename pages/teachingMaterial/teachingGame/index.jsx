820e9185505b947dcaafd3d2e6e73101730985e5
BinhVH <binh.vh2311@gmail.com>
2024-02-29 12:26:11 +0700

    add download link

--- pages/teachingMaterial/teachingGame/index.jsx
+++ pages/teachingMaterial/teachingGame/index.jsx
@@ -24,7 +24,7 @@ const games = [
         name: 'Class Room Object',
         des: 'Is a game',
         img: '/gameImg/classRomeObject.png',
-        link: '',
+        link: 'https://docs.google.com/presentation/d/1BicNdjAO7HtcwutuG-eMvfTvam9syvhM/edit?usp=sharing&ouid=105162719495193298319&rtpof=true&sd=true',
     },
     {
         id: 1,
@@ -32,7 +32,7 @@ const games = [
         name: 'Christmas Vocalbulary',
         des: 'Is a game',
         img: '/gameImg/chrimasVocalbulary.png',
-        link: '',
+        link: 'https://docs.google.com/presentation/d/1wnmPvt-ijFMT3WCCBMIV3_xdLD21Y44Z/edit?usp=sharing&ouid=105162719495193298319&rtpof=true&sd=true',
     },
     {
         id: 2,
@@ -40,7 +40,7 @@ const games = [
         name: 'Directions Driving',
         des: 'Is a game',
         img: '/gameImg/directionsDriving.png',
-        link: '',
+        link: 'https://docs.google.com/presentation/d/10Rwa6x-O9Y3OMGYpMBhCKAWkTTy2VI7_/edit?usp=sharing&ouid=105162719495193298319&rtpof=true&sd=true',
     },
     {
         id: 3,
@@ -48,7 +48,7 @@ const games = [
         name: 'Simple Past Tense',
         des: 'Is a game',
         img: '/gameImg/simplePastTense.png',
-        link: '',
+        link: 'https://docs.google.com/presentation/d/1605rTThfL13lZWq45sHfVLjvu1NLc_Mu/edit?usp=sharing&ouid=105162719495193298319&rtpof=true&sd=true',
     },
     {
         id: 4,
@@ -56,7 +56,7 @@ const games = [
         name: 'Discover The Animals',
         des: 'Is a game',
         img: '/gameImg/discoverTheAnimals.png',
-        link: '',
+        link: 'https://docs.google.com/presentation/d/1Hl_PBMHLa4MN-UFy7z8TuDnrc4-qE1zz/edit?usp=drive_link&ouid=105162719495193298319&rtpof=true&sd=true',
     },
     {
         id: 5,
@@ -64,7 +64,7 @@ const games = [
         name: 'Get Dressed',
         des: 'Is a game',
         img: '/gameImg/getDressed.png',
-        link: '',
+        link: 'https://docs.google.com/presentation/d/11GrerhRYM8ddB4_LMwLsbT8kcHjHYgEu/edit?usp=drive_link&ouid=105162719495193298319&rtpof=true&sd=true',
     },
     {
         id: 6,
@@ -72,7 +72,7 @@ const games = [
         name: 'Giving Directions',
         des: 'Is a game',
         img: '/gameImg/givingDirections.png',
-        link: '',
+        link: 'https://docs.google.com/presentation/d/1JmgBrFl3m0ddEmGV3IpdTEmObhs-eaF-/edit?usp=drive_link&ouid=105162719495193298319&rtpof=true&sd=true',
     },
     {
         id: 7,
@@ -80,7 +80,7 @@ const games = [
         name: 'Class Room Object',
         des: 'Is a game',
         img: '/gameImg/classRomeObject.png',
-        link: '',
+        link: 'https://docs.google.com/presentation/d/1BicNdjAO7HtcwutuG-eMvfTvam9syvhM/edit?usp=sharing&ouid=105162719495193298319&rtpof=true&sd=true',
     },
     {
         id: 8,
@@ -88,7 +88,7 @@ const games = [
         name: 'Irregular Verb Machine',
         des: 'Is a game',
         img: '/gameImg/irregularVerbMachine.png',
-        link: '',
+        link: 'https://docs.google.com/presentation/d/1t5Z3RjE0UIYVB5fWu-FhuSCC4MQE5khi/edit?usp=drive_link&ouid=105162719495193298319&rtpof=true&sd=true',
     },
     {
         id: 9,
@@ -96,7 +96,7 @@ const games = [
         name: 'Ai Là Triệu Phú',
         des: 'Is a game',
         img: '/gameImg/aiLaTrieuPhu.png',
-        link: '',
+        link: 'https://docs.google.com/presentation/d/1SF_fRYk2STSo5k_Sv1yWbz8aCPAQgkqv/edit?usp=sharing&ouid=105162719495193298319&rtpof=true&sd=true',
     },
     {
         id: 10,
@@ -104,7 +104,7 @@ const games = [
         name: 'Đào Vàng',
         des: 'Is a game',
         img: '/gameImg/daoVang.png',
-        link: '',
+        link: 'https://docs.google.com/presentation/d/1PUUsyYV3C-y75dQQBSYvB_ohb305ALZQ/edit?usp=drive_link&ouid=105162719495193298319&rtpof=true&sd=true',
     },
     {
         id: 11,
@@ -112,7 +112,7 @@ const games = [
         name: 'Giải Cứu Đại Dương',
         des: 'Is a game',
         img: '/gameImg/giaiCuuDaiDuong.png',
-        link: '',
+        link: 'https://docs.google.com/presentation/d/17cqqTxNFFOqggweDeN1lTwjKX9pBONzh/edit?usp=drive_link&ouid=105162719495193298319&rtpof=true&sd=true',
     },
     {
         id: 12,
@@ -120,7 +120,7 @@ const games = [
         name: 'Rung Chuông Vàng',
         des: 'Is a game',
         img: '/gameImg/rungChuongVang.png',
-        link: '',
+        link: 'https://docs.google.com/presentation/d/1jAK9Z7R8Rf11KfiZ-RLOy8unLHFd7Dld/edit?usp=drive_link&ouid=105162719495193298319&rtpof=true&sd=true',
     },
     {
         id: 13,
@@ -128,7 +128,7 @@ const games = [
         name: 'Vòng Quay Kì Diệu',
         des: 'Is a game',
         img: '/gameImg/vongQuayKiDieu.png',
-        link: '',
+        link: 'https://docs.google.com/presentation/d/1Wlbpsm6QITmtUCg8O3wQyu3vSLXHH67B/edit?usp=drive_link&ouid=105162719495193298319&rtpof=true&sd=true',
     },
     {
         id: 14,
@@ -136,7 +136,7 @@ const games = [
         name: 'Chuột Hamster',
         des: 'Is a game',
         img: '/gameImg/chuotHamster.png',
-        link: '',
+        link: 'https://docs.google.com/presentation/d/1w7yohoROp0io3Jqzp6dRpQ0krcGvHFJ0/edit?usp=drive_link&ouid=105162719495193298319&rtpof=true&sd=true',
     },
     {
         id: 15,
@@ -144,7 +144,7 @@ const games = [
         name: 'Cọp ơi cậu ở đâu thế',
         des: 'Is a game',
         img: '/gameImg/copOi.png',
-        link: '',
+        link: 'https://docs.google.com/presentation/d/1BHthfObBTyVRO7-b3XIMh5GiO0g1qP3n/edit?usp=drive_link&ouid=105162719495193298319&rtpof=true&sd=true',
     },
     {
         id: 16,
@@ -152,7 +152,7 @@ const games = [
         name: 'Modal Web',
         des: 'Is a game',
         img: '/gameImg/modalWeb.png',
-        link: '',
+        link: 'https://docs.google.com/presentation/d/1Vr2XjFEHQnrAACE3ecuHCpeuOefsVsv4/edit?usp=drive_link&ouid=105162719495193298319&rtpof=true&sd=true',
     },
     {
         id: 17,
@@ -160,7 +160,7 @@ const games = [
         name: 'Number Machine',
         des: 'Is a game',
         img: '/gameImg/numberMachine.png',
-        link: '',
+        link: 'https://docs.google.com/presentation/d/1VIQJE3GijQz4jImM7QAB0rO3eP0Q5Fzn/edit?usp=drive_link&ouid=105162719495193298319&rtpof=true&sd=true',
     },
     {
         id: 18,
@@ -168,7 +168,7 @@ const games = [
         name: 'Milionare',
         des: 'Is a game',
         img: '/gameImg/milionare.png',
-        link: '',
+        link: 'https://docs.google.com/presentation/d/1FpO27zeqXjJ9IUf6hhcQ5ozSlDqU83I0/edit?usp=drive_link&ouid=105162719495193298319&rtpof=true&sd=true',
     },
     {
         id: 19,
@@ -176,7 +176,7 @@ const games = [
         name: 'Prepositions Of Time',
         des: 'Is a game',
         img: '/gameImg/prepositionsOfTime.png',
-        link: '',
+        link: 'https://docs.google.com/presentation/d/1ORwd7yFVA6qjxapJoyXBwyI0QONCuwAp/edit?usp=drive_link&ouid=105162719495193298319&rtpof=true&sd=true',
     },
     {
         id: 20,
@@ -184,7 +184,7 @@ const games = [
         name: 'Present Simple Continuous',
         des: 'Is a game',
         img: '/gameImg/presentSimpleContinuous.png',
-        link: '',
+        link: 'https://docs.google.com/presentation/d/1uAUpAQkibDtGbkveSJl2MIfgsR2lNI4T/edit?usp=drive_link&ouid=105162719495193298319&rtpof=true&sd=true',
     },
     {
         id: 21,
@@ -192,7 +192,7 @@ const games = [
         name: 'Question Words',
         des: 'Is a game',
         img: '/gameImg/questionWords.png',
-        link: '',
+        link: 'https://docs.google.com/presentation/d/1ay9tc9NaJJlPNtyqZSnt7qmCXqB9POel/edit?usp=drive_link&ouid=105162719495193298319&rtpof=true&sd=true',
     },
     {
         id: 22,
@@ -200,7 +200,7 @@ const games = [
         name: 'Routines Quiz',
         des: 'Is a game',
         img: '/gameImg/routinesQuiz.png',
-        link: '',
+        link: 'https://docs.google.com/presentation/d/1IM0gMTryitiYCMpvuT8D9NgFGrtgBFLZ/edit?usp=drive_link&ouid=105162719495193298319&rtpof=true&sd=true',
     },
     {
         id: 23,
@@ -208,7 +208,7 @@ const games = [
         name: 'Routines Time',
         des: 'Is a game',
         img: '/gameImg/routinesTime.png',
-        link: '',
+        link: 'https://docs.google.com/presentation/d/1K0HKlOHsGEZc4iMFSZb_ziZzGCrSvleO/edit?usp=drive_link&ouid=105162719495193298319&rtpof=true&sd=true',
     },
     {
         id: 24,
@@ -216,7 +216,7 @@ const games = [
         name: 'Spot The Difference',
         des: 'Is a game',
         img: '/gameImg/spotTheDifference.png',
-        link: '',
+        link: 'https://docs.google.com/presentation/d/1GUcn7nmP-QIgOcCWYst4YEm8L1LMisFM/edit?usp=drive_link&ouid=105162719495193298319&rtpof=true&sd=true',
     },
     {
         id: 25,
@@ -224,7 +224,7 @@ const games = [
         name: 'Must Mustnt',
         des: 'Is a game',
         img: '/gameImg/mustMustnt.png',
-        link: '',
+        link: 'https://docs.google.com/presentation/d/1f8U5JffirkZj2u5GUHgWQai8nDPyF-j2/edit?usp=drive_link&ouid=105162719495193298319&rtpof=true&sd=true',
     },
     {
         id: 26,
@@ -232,7 +232,7 @@ const games = [
         name: 'Part Of The Body',
         des: 'Is a game',
         img: '/gameImg/partOfTheBody.png',
-        link: '',
+        link: 'https://docs.google.com/presentation/d/1cZvG6Sua7Jnhj69V0YF0gaKRcMdjwEKs/edit?usp=sharing&ouid=105162719495193298319&rtpof=true&sd=true',
     },
     {
         id: 27,
@@ -240,7 +240,7 @@ const games = [
         name: 'Animals Guess',
         des: 'Is a game',
         img: '/gameImg/animalsGuess.png',
-        link: '',
+        link: 'https://docs.google.com/presentation/d/1sWe8kmY1_HdrBCST-jsxXLVPi_0kbbvp/edit?usp=sharing&ouid=105162719495193298319&rtpof=true&sd=true',
     },
     {
         id: 28,
@@ -248,7 +248,7 @@ const games = [
         name: 'Compartives And Superlatives',
         des: 'Is a game',
         img: '/gameImg/compartivesAndSuperlatives.png',
-        link: '',
+        link: 'https://docs.google.com/presentation/d/1WBYJv3Qye77MbGf_KWSPWKs1789bWQEh/edit?usp=sharing&ouid=105162719495193298319&rtpof=true&sd=true',
     },
     {
         id: 29,
@@ -256,7 +256,7 @@ const games = [
         name: 'Daily Routines',
         des: 'Is a game',
         img: '/gameImg/dailyRoutines.png',
-        link: '',
+        link: 'https://docs.google.com/presentation/d/1vFuoNC7GUmHMq1CI2dZ5B8M_60sr66LV/edit?usp=drive_link&ouid=105162719495193298319&rtpof=true&sd=true',
     },
     {
         id: 30,
@@ -264,7 +264,7 @@ const games = [
         name: 'Old MacDonald Farm',
         des: 'Is a game',
         img: '/gameImg/oldMacDonaldFarm.png',
-        link: '',
+        link: 'https://docs.google.com/presentation/d/15wBT7SbIdR5maxE5f2Z6NZe1HVuJHqFs/edit?usp=sharing&ouid=105162719495193298319&rtpof=true&sd=true',
     },
     {
         id: 31,
@@ -272,7 +272,7 @@ const games = [
         name: 'Fast Food Wheel',
         des: 'Is a game',
         img: '/gameImg/fastFoodWheel.png',
-        link: '',
+        link: 'https://docs.google.com/presentation/d/1UWRWZJvoSZzsO7pMGoFDT_3BuzT3RhqZ/edit?usp=sharing&ouid=105162719495193298319&rtpof=true&sd=true',
     },
     {
         id: 32,
@@ -280,7 +280,7 @@ const games = [
         name: 'Jeoparody',
         des: 'Is a game',
         img: '/gameImg/jeoparody.png',
-        link: '',
+        link: 'https://docs.google.com/presentation/d/1SnsIsiq-Tg829GotoW3J-3CSPps_mzTQ/edit?usp=drive_link&ouid=105162719495193298319&rtpof=true&sd=true',
     },
     {
         id: 33,
@@ -288,7 +288,7 @@ const games = [
         name: 'Mr HowMuch Vs Mr How Many',
         des: 'Is a game',
         img: '/gameImg/mrHowMuchVsMrHowMany.png',
-        link: '',
+        link: 'https://docs.google.com/presentation/d/1ZykiUwA2vkgSHuKdsPEp6CLgdtgnGBTM/edit?usp=drive_link&ouid=105162719495193298319&rtpof=true&sd=true',
     },
 ]
 export default function TeachingGame() {
@@ -356,7 +356,7 @@ export default function TeachingGame() {
                                             {g.des}
                                         </div>
                                         <div  className={styles.gameDownLoad}>
-                                            Tải về
+                                            <a className={styles.downloadLink} href={g.link} target='_blank'>Tải về</a>
                                         </div>
                                     </div>
                                 )}
