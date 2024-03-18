1b488c2c9f0fcd327f1c61ff8338c2c002221a3a
BinhVH <binh.vh2311@gmail.com>
2024-03-18 10:19:30 +0700

    fix css

--- pages/teachingMaterial/components/header/header.jsx
+++ pages/teachingMaterial/components/header/header.jsx
@@ -4,9 +4,11 @@ import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import Pagination from '@/components/home/pagination/pagination';
 import { Modal, Button } from 'react-bootstrap';
+import { useRouter } from 'next/router';
 
 export default function Header() {
     const [showNavigation, setShowNavigation] = useState(false);
+    const router = useRouter();
     return (
         <>
             <Modal contentClassName={styles.myModalCT} backdropClassName={styles.myModalBD} show={showNavigation} onHide={()=>setShowNavigation(false)}>
@@ -18,7 +20,7 @@ export default function Header() {
                         <Link className={styles.link} href="https://media-soup.vercel.app/teachingMaterial/teachingImage">Hình ảnh trực quan</Link>
                     </div>
                     <div className={styles.headerNavigation}>
-                        Video
+                        <Link className={styles.link} href="">Video</Link>
                     </div>
                     <div className={styles.headerNavigation}>
                         <Link className={styles.link} href="https://media-soup.vercel.app/teachingMaterial/teachingGame">Trò chơi học tập</Link>
@@ -30,24 +32,28 @@ export default function Header() {
             </Modal>
             <div className={styles.headerContainer}>
                 <div className={styles.logo}>
-                    <img src='https://lh3.googleusercontent.com/sZCtuI5NKeq_V0sn3AxE4u8ZPabmvIpeld3HH7dYT5EWUL0OuutlqK15gZ8ml6hyKLfuC-pPDsLINltHJi5V8FlDBqUgLpf3fQ=w57-rw'>
+                    <img style={{borderRadius: '50%'}} src='https://lh3.googleusercontent.com/sZCtuI5NKeq_V0sn3AxE4u8ZPabmvIpeld3HH7dYT5EWUL0OuutlqK15gZ8ml6hyKLfuC-pPDsLINltHJi5V8FlDBqUgLpf3fQ=w57-rw'>
 
                     </img>
                 </div>
                 <div className={styles.headerNavigation}>
-                    <Link className={styles.link} href="https://media-soup.vercel.app/teachingMaterial">Trang chủ</Link>
+                    <Link 
+                        className={`${styles.link}`} 
+                        href="https://media-soup.vercel.app/teachingMaterial">
+                        Trang chủ
+                    </Link>
                 </div>
                 <div className={styles.headerNavigation}>
-                    <Link className={styles.link} href="https://media-soup.vercel.app/teachingMaterial/teachingImage">Hình ảnh trực quan</Link>
+                    <Link className={`${styles.link} ${router.pathname.includes('teachingImage') && styles.botBotder}`} href="https://media-soup.vercel.app/teachingMaterial/teachingImage">Hình ảnh trực quan</Link>
                 </div>
                 <div className={styles.headerNavigation}>
-                    Video
+                    <Link className={`${styles.link} ${router.pathname.includes('teachingVideo') && styles.botBotder}`} href="">Video</Link>
                 </div>
                 <div className={styles.headerNavigation}>
-                    <Link className={styles.link} href="https://media-soup.vercel.app/teachingMaterial/teachingGame">Trò chơi học tập</Link>
+                    <Link className={`${styles.link} ${router.pathname.includes('teachingGame') && styles.botBotder}`} href="https://media-soup.vercel.app/teachingMaterial/teachingGame">Trò chơi học tập</Link>
                 </div>
                 <div className={styles.headerNavigation}>
-                    <Link className={styles.link} href="https://media-soup.vercel.app/teachingMaterial/teachingDocument">Tài liệu giảng dạy</Link>
+                    <Link className={`${styles.link} ${router.pathname.includes('teachingDocument') && styles.botBotder}`} href="https://media-soup.vercel.app/teachingMaterial/teachingDocument">Tài liệu giảng dạy</Link>
                 </div>	
             </div>
             <div className={styles.headerMobile}>
